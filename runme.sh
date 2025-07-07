5acbaee47a312244f1dc3c52c9d63527f5936352
Daniel Zoba <d.zoba@zalozba.de>
2025-07-07 08:54:56 +0200

    remember install and add original subs

--- runme.sh
+++ runme.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# install local version like this 
+# pip install . --break-system-packages
+# (better to use a local venv in the future)
+
+# export PATH=$PATH:$HOME/.local/bin
+
 export FILENAME=$1
 export HF_TOKEN=$2
 export OUTDIR=$3
@@ -12,7 +18,7 @@ open-dubbing --input_file $FILENAME --source_language deu --target_language hsb 
 --hugging_face_token $HF_TOKEN --output_directory $OUTDIR \
 --translator sotra --apertium_server http://172.26.0.3:3000/translate \
 --tts bamborak --tts_api_server https://bamborakapi.mudrowak.de/api/tts/ \
---dubbed_subtitles
+--dubbed_subtitles --original_subtitles
 
 
 
